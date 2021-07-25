64a87161a4e7d2c7e147ae78e77c1f9b5bfc515b
Евгений <samsonnik-zheny@mail.ru>
2021-07-25 16:04:22 +0300

    Homework 3, add Date Validator

---
+++ src/lesson3/homework/DateValidator.java
@@ -0,0 +1,65 @@
+package lesson3.homework;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateValidator {
+    // date dd/MM/yyyy, where [0-31]/[01-12]/[1990 - 2099]
+    private static final Pattern dateType1 = Pattern.compile("^(0[1-9]|[1-2][1-9]|3[0-1])/(0[1-9]|1[1-2])/(199[0-9]|20[0-9][0-9])$");
+    // date dd-MM-yyyy, where [0-31]/[01-12]/[1990 - 2099]
+    private static final Pattern dateType2 = Pattern.compile("^(0[1-9]|[1-2][1-9]|3[0-1])-(0[1-9]|1[1-2])-(199[0-9]|20[0-9][0-9])$");
+
+    public static void dataCheck(String stringDate) throws ParseException {
+
+        Matcher matcherDataType1 = dateType1.matcher(stringDate);
+        Matcher matcherDataType2 = dateType2.matcher(stringDate);
+        StringBuilder stringBuilder = new StringBuilder();
+        Date dateCheck = new Date();
+
+        if (matcherDataType1.matches()) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = simpleDateFormat.parse(stringDate);
+            if (date.after(dateCheck)) {
+                System.out.println("Your date has not come yet");
+            } else {
+                Calendar calendar = new GregorianCalendar();
+                calendar.setTime(date);
+                stringBuilder.append("День: <").append(calendar.get(Calendar.DAY_OF_MONTH)).append(">");
+                System.out.println(stringBuilder);
+                stringBuilder.setLength(0);
+                stringBuilder.append("Месяц: <").append(calendar.get(Calendar.MONTH) + 1).append(">");
+                System.out.println(stringBuilder);
+                stringBuilder.setLength(0);
+                stringBuilder.append("Year: <").append(calendar.get(Calendar.YEAR)).append(">");
+                System.out.println(stringBuilder);
+            }
+        } else if (matcherDataType2.matches()) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            Date date = simpleDateFormat.parse(stringDate);
+            if (date.after(dateCheck)) {
+                System.out.println("Your date has not come yet");
+            } else {
+                Calendar calendar = new GregorianCalendar();
+                calendar.setTime(date);
+                stringBuilder.append("День: <").append(calendar.get(Calendar.DAY_OF_MONTH)).append(">");
+                System.out.println(stringBuilder);
+                stringBuilder.setLength(0);
+                stringBuilder.append("Месяц: <").append(calendar.get(Calendar.MONTH) + 1).append(">");
+                System.out.println(stringBuilder);
+                stringBuilder.setLength(0);
+                stringBuilder.append("Year: <").append(calendar.get(Calendar.YEAR)).append(">");
+                System.out.println(stringBuilder);
+
+            }
+        } else {
+            System.out.println("Check your insert, matches not found");
+        }
+    }
+}
+
+
--- src/lesson3/homework/Source.java
+++ src/lesson3/homework/Source.java
@@ -1,10 +1,11 @@
 package lesson3.homework;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Source {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Product firstTv = new TV("A45", 9_000, 3,
                 "Samsung", 49.00, true);
         Product firstPC = new Computer("Nagibator3000", 20_000.0, 2,
@@ -20,9 +21,10 @@ public class Source {
         User seller = new User("Vasyan", 100_000);
         User buyer = new User("Igoryan", 50_000);
 
-        System.out.println(firstTv.averagePrice());
-        System.out.println(firstTv.discount());
-        System.out.println(firstTv.toString());
+        DateValidator.dataCheck("12/05/2021");
+        DateValidator.dataCheck("12-07-2023");
+
+
 
     }
 }
